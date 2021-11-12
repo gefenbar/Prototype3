52e96c085d1ff1f9b34ee4effc38017fc1e9bfd5
Gefen <gefenbar009@gmail.com>
2021-11-12 14:34:20 +0200

    Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,26 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 6.0f;
+    [SerializeField]
+    private float speed = 2.0f;
 
-    private void Update()
+    [SerializeField]
+    private float jumpForce = 5.0f;
+
+    private Rigidbody2D rigidBody;
+    private void Start()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -= transform.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += transform.right * speed * Time.deltaTime;
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector2 movement = new Vector2(deltaX, rigidBody.velocity.y);
+        rigidBody.velocity = movement;
+
+        if (!Mathf.Approximately(deltaX, 0.0f))
+        {
+            // Scale x to either positive or negative 1 to 'turn' the character
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1.0f, 1.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }
+
