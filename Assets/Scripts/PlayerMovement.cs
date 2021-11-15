2e8157a7b5e32ca48557b38d176b15d6e9991362
omer2695 <82286519+omer2695@users.noreply.github.com>
2021-11-15 12:21:27 +0200

    ExtraJump + Destroy meat

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float speed = 100.0f;
-
     [SerializeField]
     private float jumpForce = 4.0f;
+    private bool Jumping;
+    private int ExtraJump;
     private Rigidbody2D rigidBody;
     public Animator animator;
+   
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -32,21 +34,39 @@ public class PlayerMovement : MonoBehaviour
             // Scale x to either positive or negative 1 to 'turn' the character
             transform.localScale = new Vector3(Mathf.Sign(deltaX)/3.2f, 0.35f, 0.35f);
         }
-        
-        
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if ((Input.GetKeyDown(KeyCode.Space) && !Jumping) || (Input.GetKeyDown(KeyCode.Space) && ExtraJump > 0))
         {
             rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             animator.SetBool("IsJumping", true);
-            
+            Jumping = true;
+            ExtraJump--;
         }
         else
         {
             animator.SetBool("IsJumping", false);
+            
         }
        
     }
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Meat"))
+        {
+            Destroy(other.gameObject);
+
+
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Jumping = false;
+            ExtraJump = 2;
+        }
+    }
+
 
 
 }
